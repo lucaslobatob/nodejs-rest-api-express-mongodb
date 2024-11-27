e7229a2e4ac826a6d119c5d7eb76d9fe9e9f8b77
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2024-11-27 08:19:35 -0300

    Created function buscaLivros and add put method

--- src/app.js
+++ src/app.js
@@ -12,7 +12,14 @@ let livros = [
         "id": 2,
         "name": "O Hobbit",
     }
-]
+];
+
+function buscaLivros(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    }
+    );
+};
 
 app.get("/", (req, res) => {
     res.status(200).send("Curso Node.js");
@@ -22,9 +29,20 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivros(req.params.id);
+    res.json(livros[index]);
+});
+
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("Livro cadastrado com sucesso");
-})
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivros(req.params.id);
+    livros[index].name = req.body.name;
+    res.status(200).json(livros);
+});
 
 export default app;
