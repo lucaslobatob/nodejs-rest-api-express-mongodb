5025d9529f68278060c7b7a954ba4ddf4ebc0ccb
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2024-11-27 07:39:52 -0300

    Add livros route

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from 'express';
 
 const app = express();
 
+let livros = [
+    {
+        "id": 1,
+        "name": 'Senhor dos Aneis',
+    },
+    {
+        "id": 2,
+        "name": 'O Hobbit',
+    }
+]
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso Node.js");
 });
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+})
 
 export default app;
