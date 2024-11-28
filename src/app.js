f5533ac3917113f12968f8c7d6190020cf7adec0
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2024-11-28 15:38:45 -0300

    Add dataBase connection in app

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import conectarNaDataBase from './config/dbConnect.js';
+
+const conexao = await conectarNaDataBase();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco feito com sucesso!");
+});
 
 const app = express();
 app.use(express.json());
