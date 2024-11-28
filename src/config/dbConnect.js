53df819ba36275713b2697307abefdf924daae9b
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2024-11-28 16:01:23 -0300

    Att dataBase

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose, { mongo } from "mongoose";
 
 async function conectarNaDataBase() {
-    mongoose.connect("mongodb+srv://admin:admin123@cluster0.nmabd.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0");
+    mongoose.connect(process.env.DB_CONNECTION_STRING);
 
     return mongoose.connection;
 };
