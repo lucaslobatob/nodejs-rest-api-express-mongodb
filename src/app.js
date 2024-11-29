d5679d229679df0fb479819ee08db7664b155ddb
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2024-11-29 14:47:11 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,22 +15,6 @@ conexao.once("open", () => {
 const app = express();
 routes(app);
 
-app.get("/livros/:id", (req, res) => {
-    const index = buscaLivros(req.params.id);
-    res.json(livros[index]);
-});
-
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("Livro cadastrado com sucesso");
-});
-
-app.put("/livros/:id", (req, res) => {
-    const index = buscaLivros(req.params.id);
-    livros[index].name = req.body.name;
-    res.status(200).json(livros);
-});
-
 app.delete("/livros/:id", (req, res) => {
     const index = buscaLivros(req.params.id);
     livros.splice(index, 1);
