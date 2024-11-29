8fb0c45f5646f3809a08483f00332e567646bd02
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2024-11-29 14:48:22 -0300

    Add get book id and put book

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -3,8 +3,22 @@ import livro from "../models/Livro.js";
 class LivroController {
 
     static async listarLivros (req, res) {
+        try{
             const listaLivros = await livro.find({});
             res.status(200).json(listaLivros);
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha da requisição` });
+        }
+    };
+
+    static async listarLivroPorId (req, res) {
+        try{
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+            res.status(200).json(livroEncontrado);
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha da requisição do livro` });
+        }
     };
 
     static async cadastrarLivro (req, res) {
@@ -14,7 +28,17 @@ class LivroController {
         } catch (error) {
             res.status(500).json({ message: `${error.message} - falha ao cadastrar livro` });
         }
-    }
+    };
+
+    static async atualizarLivro (req, res) {
+        try{
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: "Livro atualizado" });
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha na atualização` });
+        }
+    };
 };
 
 export default LivroController;
