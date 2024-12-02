38a239bec1d64c864030423a0dc3902ba83e3f44
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2024-12-02 15:07:55 -0300

    Created function listarLivrosPorEditora

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -53,6 +53,16 @@ class LivroController {
             res.status(500).json({ message: `${error.message} - falha ao deletar livro` });
         }
     };
+
+    static async listarLivrosPorEditora (req, res) {
+        const editora = req.query.editora;
+        try {
+            const livrosPorEditora = await livro.find({ editora: editora });
+            res.status(200).json(livrosPorEditora);
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha a busca` });
+        }
+    }
 };
 
 export default LivroController;
